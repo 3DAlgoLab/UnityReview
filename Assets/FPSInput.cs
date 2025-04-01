ba4b6d6bdf57153d1f6d85d54cfa954c9760f231
3DAlgoLab <threealgo@outlook.com>
2025-04-01 21:58:48 +0900

    Add FPS input script

--- Assets/FPSInput.cs
+++ Assets/FPSInput.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
-    public float speed = 1.0f; 
+    public float speed = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -14,10 +14,10 @@ public class FPSInput : MonoBehaviour
     {
         // transform.Translate(0, speed, 0);
 
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
+        var deltaX = Input.GetAxis("Horizontal") * speed;
+        var deltaZ = Input.GetAxis("Vertical") * speed;
 
-        float t_delta = Time.deltaTime; 
-        transform.Translate(deltaX*t_delta, 0, deltaZ*t_delta);
+        var t_delta = Time.deltaTime;
+        transform.Translate(deltaX * t_delta, 0, deltaZ * t_delta);
     }
 }
